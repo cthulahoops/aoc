9c90d590b9f697bcc963b54405c8b39d57ea6409
Adam Kelly <adam@cthulahoops.org>
2020-12-02 23:30:28 +0000

    Refactor day 2

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,70 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 
-fn char_count(text: &str, x: char) -> i64 {
-    let mut count = 0;
-    for character in text.chars() {
-        if character == x {
-            count+= 1;
-        }
-    }
-    count
+fn char_count(text: &str, x: char) -> usize {
+    text.chars().filter(|&character| character == x).count()
 }
 
-fn is_char_at(text: &str, needle: char, position: i64) -> bool {
-    let char_at = text.chars().nth((position as usize - 1) as usize).unwrap();
+fn is_char_at(text: &str, needle: char, position: usize) -> bool {
+    let char_at = text.chars().nth(position - 1).unwrap();
     char_at == needle
+}
+
+lazy_static! {
+    static ref PASSWORD_LINE_FORMAT: Regex = Regex::new(r"^(\d+)-(\d+) (\w): (.+)$").unwrap();
+}
+
+struct Policy {
+    min: usize,
+    max: usize,
+    character: char,
+}
+
+fn parse_line(line: &str) -> (Policy, &str) {
+    let captures = PASSWORD_LINE_FORMAT.captures(line).unwrap();
+    let min: usize = captures.get(1).unwrap().as_str().parse().unwrap();
+    let max: usize = captures.get(2).unwrap().as_str().parse().unwrap();
+    let character = captures.get(3).unwrap().as_str().chars().next().unwrap();
+    let password = captures.get(4).unwrap().as_str();
+
+    (
+        Policy {
+            min,
+            max,
+            character,
+        },
+        password,
+    )
+}
+
+impl Policy {
+    fn has_valid_count(&self, password: &str) -> bool {
+        let count = char_count(password, self.character);
+        count >= self.min && count <= self.max
+    }
+
+    fn has_valid_positional(&self, password: &str) -> bool {
+        let cond1 = is_char_at(password, self.character, self.min);
+        let cond2 = is_char_at(password, self.character, self.max);
+        (cond1 || cond2) && !(cond1 && cond2)
+    }
 }
 
 fn main() {
     let content = std::fs::read_to_string("input/day2-1").unwrap();
-    let password_line_format = Regex::new(r"^(\d+)-(\d+) (\w): (.+)$").unwrap();
 
     let mut valid_passwords_part1 = 0;
     let mut valid_passwords_part2 = 0;
 
     for line in content.lines() {
-        println!("{}", line);
-        let captures = password_line_format.captures(line).unwrap();
-        let min : i64 = captures.get(1).unwrap().as_str().parse().unwrap();
-        let max : i64 = captures.get(2).unwrap().as_str().parse().unwrap();
-        let character = captures.get(3).unwrap().as_str().chars().next().unwrap();
-        let password = captures.get(4).unwrap().as_str();
+        let (policy, password) = parse_line(line);
 
-        let count = char_count(password, character);
-
-        if count >= min && count <= max {
+        if policy.has_valid_count(password) {
             valid_passwords_part1 += 1;
-            println!("VALID");
         }
 
-        let cond1 = is_char_at(password, character, min);
-        let cond2 = is_char_at(password, character, max);
-        if (cond1 || cond2) && !(cond1 && cond2) {
+        if policy.has_valid_positional(password) {
             valid_passwords_part2 += 1;
-            println!("VALID 2");
         }
     }
     println!("Valid passwords (part 1) = {}", valid_passwords_part1);
