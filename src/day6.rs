785253307760459b51d9cbe7313349030142771d
Adam Kelly <adam@cthulahoops.org>
2020-12-07 08:47:35 +0000

    Formatting.

--- src/day6.rs
+++ src/day6.rs
@@ -12,7 +12,8 @@ fn group_answers<F: Fn(&Answers, &Answers) -> Answers>(
 ) -> HashSet<char> {
     let (first_answers, rest_answers) = group.split_first().unwrap();
 
-    rest_answers.iter()
+    rest_answers
+        .iter()
         .fold(first_answers.clone(), |group_answers, answer| {
             op(&group_answers, answer)
         })
@@ -25,22 +26,21 @@ fn sum_counts<I: Iterator<Item = Answers>>(grouped_answers: I) -> usize {
 fn main() {
     let content = std::fs::read_to_string("input/day6").unwrap();
 
-    let groups = content.split("\n\n").map(|group| group.trim_end().split('\n').map(answers).collect());
+    let groups = content
+        .split("\n\n")
+        .map(|group| group.trim_end().split('\n').map(answers).collect());
 
-    let group_answers_1 = groups
-        .clone()
-        .map(|group| {
-            group_answers(&group, |group_answers, answers| {
-                group_answers.union(answers).cloned().collect()
-            })
-        });
+    let group_answers_1 = groups.clone().map(|group| {
+        group_answers(&group, |group_answers, answers| {
+            group_answers.union(answers).cloned().collect()
+        })
+    });
 
-    let group_answers_2 = groups
-        .map(|group| {
-            group_answers(&group, |group_answers, answers| {
-                group_answers.intersection(answers).cloned().collect()
-            })
-        });
+    let group_answers_2 = groups.map(|group| {
+        group_answers(&group, |group_answers, answers| {
+            group_answers.intersection(answers).cloned().collect()
+        })
+    });
 
     println!("Total = {}", sum_counts(group_answers_1));
     println!("Total = {}", sum_counts(group_answers_2));
