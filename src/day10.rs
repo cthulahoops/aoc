15e5742e29e8ca666f38d7b27708211aa4104022
Adam Kelly <adam@cthulahoops.org>
2020-12-10 10:58:53 +0000

    Day 10 - generalisation memoisation.

--- src/day10.rs
+++ src/day10.rs
@@ -1,21 +1,70 @@
-use std::collections::HashSet;
+#![feature(unboxed_closures, fn_traits)]
+
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::hash::Hash;
 
 fn parse_sorted_numbers(contents: &str) -> Vec<i64> {
-    let mut numbers : Vec<i64> = contents.lines().map(|x| x.parse().unwrap()).collect();
+    let mut numbers: Vec<i64> = contents.lines().map(|x| x.parse().unwrap()).collect();
     numbers.sort();
     numbers
 }
 
-fn arrangements(cache: &mut HashMap<i64, i64>, adaptors: &HashSet<i64>, n: i64) -> i64 {
-    if let Some(value) = cache.get(&n) {
-        return value.clone();
-    }
+struct HashCache<A, B, R> {
+    cache: HashMap<A, R>,
+    func: fn(&mut HashCache<A, B, R>, B, A) -> R,
+}
 
+impl<A, B, R> HashCache<A, B, R>
+where
+    A: Eq + Hash,
+{
+    fn from_func(func: fn(&mut Self, B, A) -> R) -> Self {
+        HashCache {
+            cache: HashMap::new(),
+            func: func,
+        }
+    }
+}
+
+impl<A, B, R> FnMut<(B, A)> for HashCache<A, B, R>
+where
+    A: Eq + Hash + Clone,
+    R: Clone,
+{
+    extern "rust-call" fn call_mut(&mut self, args: (B, A)) -> Self::Output {
+        let arg = args.1;
+
+        if let Some(result) = self.cache.get(&arg).map(|x| x.clone()) {
+            return result;
+        }
+
+        let result = (self.func.clone())(self, args.0, arg.clone());
+        self.cache.insert(arg, result.clone());
+        result
+    }
+}
+
+// We need to implement `FnOnce` to implement `FnMut`.
+impl<A, B, R> FnOnce<(B, A)> for HashCache<A, B, R>
+where
+    A: Eq + Hash + Clone,
+    R: Clone,
+{
+    type Output = R;
+    extern "rust-call" fn call_once(mut self, args: (B, A)) -> Self::Output {
+        self.call_mut(args)
+    }
+}
+
+fn arrangements<'a, F>(recurse: &mut F, adaptors: &'a HashSet<i64>, n: i64) -> i64
+where
+    F: FnMut(&'a HashSet<i64>, i64) -> i64,
+{
     let mut count = 0;
     for x in 1..4 {
         if adaptors.contains(&(n - x)) {
-            count += arrangements(cache, adaptors, n - x);
+            count += recurse(adaptors, n - x);
         }
     }
 
@@ -23,12 +72,8 @@ fn arrangements(cache: &mut HashMap<i64, i64>, adaptors: &HashSet<i64>, n: i64) 
         count += 1;
     }
 
-    cache.insert(n, count);
-
     count
 }
-
-
 
 fn main() {
     let contents = std::fs::read_to_string("input/day10").unwrap();
@@ -50,11 +95,16 @@ fn main() {
 
     threes += 1; // Yuck!
 
-    println!("Ones = {}\nThrees = {}\nPart 1 = {}", ones, threes, ones * threes);
+    println!(
+        "Ones = {}\nThrees = {}\nPart 1 = {}",
+        ones,
+        threes,
+        ones * threes
+    );
 
     let target = last + 3;
-    let adaptors : HashSet<i64> = numbers.iter().cloned().collect();
-   
-    let mut cache = HashMap::new();
-    println!("Part 2 = {}\n", arrangements(&mut cache, &adaptors, target));
+    let adaptors: HashSet<i64> = numbers.iter().cloned().collect();
+
+    let mut memoized = HashCache::from_func(arrangements);
+    println!("Part 2 = {}\n", memoized(&adaptors, target));
 }
