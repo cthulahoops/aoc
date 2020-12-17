e7981632ac07ba53ad3bd3d74494ba52c50ce84d
Adam Kelly <adam@cthulahoops.org>
2020-12-17 20:38:33 +0000

    Use traits for Neighbour and FromPair.

--- src/day17.rs
+++ src/day17.rs
@@ -1,43 +1,7 @@
-use aoclib::Vec4;
-use lazy_static::lazy_static;
+use aoclib::{Vec4, Vec3, Neighbours, FromPair};
 use std::collections::{HashMap, HashSet};
-
-type Offsets = Vec<Vec4<i64>>;
-
-fn offsets3() -> Offsets {
-    let mut results = vec![];
-    for x in -1..2 {
-        for y in -1..2 {
-            for z in -1..2 {
-                if x != 0 || y != 0 || z != 0 {
-                    results.push(Vec4::new(x, y, z, 0));
-                }
-            }
-        }
-    }
-    results
-}
-
-fn offsets4() -> Offsets {
-    let mut results = vec![];
-    for x in -1..2 {
-        for y in -1..2 {
-            for z in -1..2 {
-                for w in -1..2 {
-                    if x != 0 || y != 0 || z != 0 || w != 0 {
-                        results.push(Vec4::new(x, y, z, w));
-                    }
-                }
-            }
-        }
-    }
-    results
-}
-
-lazy_static! {
-    static ref NEIGHBOUR_OFFSETS_3: Offsets = offsets3();
-    static ref NEIGHBOUR_OFFSETS_4: Offsets = offsets4();
-}
+use std::ops::Add;
+use std::hash::Hash;
 
 fn parse_line(s: &str) -> anyhow::Result<HashSet<usize>> {
     let row = s
@@ -48,24 +12,21 @@ fn parse_line(s: &str) -> anyhow::Result<HashSet<usize>> {
     Ok(row)
 }
 
-fn rows_to_grid(rows: Vec<HashSet<usize>>) -> Conway {
+fn rows_to_grid<T>(rows: Vec<HashSet<usize>>) -> HashSet<T> where T: Eq + FromPair + Hash {
     let mut result = HashSet::new();
     for (y, row) in rows.iter().enumerate() {
         for x in row {
-            result.insert(Vec4::new(*x as i64, y as i64, 0, 0));
+            result.insert(T::from_pair(*x as i64, y as i64));
         }
     }
     result
 }
 
-type Conway = HashSet<Vec4<i64>>;
-
-fn step(input: &Conway, offsets: &Offsets) -> Conway {
-    let mut heat: HashMap<Vec4<i64>, usize> = HashMap::new();
+fn step<T>(input: &HashSet<T>) -> HashSet<T> where T: Add<Output=T> + Hash + Eq + Copy + Neighbours {
+    let mut heat = HashMap::new();
 
     for cube in input {
-        for offset in offsets.iter() {
-            let v = *cube + *offset;
+        for v in cube.neighbours() {
             let new_x = match heat.get(&v) {
                 None => 1,
                 Some(x) => x + 1,
@@ -86,12 +47,13 @@ fn step(input: &Conway, offsets: &Offsets) -> Conway {
 
 fn main() -> anyhow::Result<()> {
     let rows = aoclib::read_parsed_lines("input/day17", parse_line)?;
-    let conway = rows_to_grid(rows);
 
-    let result = (0..6).fold(conway.clone(), |a, _i| step(&a, &NEIGHBOUR_OFFSETS_3));
+    let conway : HashSet<Vec3<i64>> = rows_to_grid(rows.clone());
+    let result = (0..6).fold(conway.clone(), |a, _i| step(&a));
     println!("Part 1 = {}", result.len());
 
-    let result = (0..6).fold(conway, |a, _i| step(&a, &NEIGHBOUR_OFFSETS_4));
+    let conway : HashSet<Vec4<i64>> = rows_to_grid(rows);
+    let result = (0..6).fold(conway, |a, _i| step(&a));
     println!("Part 2 = {}", result.len());
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 use derive_more::{Add, Mul};
+use lazy_static::lazy_static;
+
+pub trait FromPair {
+    fn from_pair(x: i64, y: i64) -> Self;
+}
+
+pub trait Neighbours {
+    fn neighbours(&self) -> Vec<Self> where Self: Sized;
+}
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Add, Mul)]
 pub struct Vec2<T> {
@@ -40,6 +49,53 @@ impl<T> Vec3<T> {
     }
 }
 
+impl FromPair for Vec3<i64> {
+    fn from_pair(x: i64, y: i64) -> Self {
+        Vec3::new(x, y, 0)
+    }
+}
+
+fn offsets3() -> Vec<Vec3<i64>> {
+    let mut results = vec![];
+    for x in -1..2 {
+        for y in -1..2 {
+            for z in -1..2 {
+                if x != 0 || y != 0 || z != 0 {
+                    results.push(Vec3::new(x, y, z));
+                }
+            }
+        }
+    }
+    results
+}
+
+fn offsets4() -> Vec<Vec4<i64>> {
+    let mut results = vec![];
+    for x in -1..2 {
+        for y in -1..2 {
+            for z in -1..2 {
+                for w in -1..2 {
+                    if x != 0 || y != 0 || z != 0 || w != 0 {
+                        results.push(Vec4::new(x, y, z, w));
+                    }
+                }
+            }
+        }
+    }
+    results
+}
+
+lazy_static! {
+    static ref NEIGHBOUR_OFFSETS_3: Vec<Vec3<i64>> = offsets3();
+    static ref NEIGHBOUR_OFFSETS_4: Vec<Vec4<i64>> = offsets4();
+}
+
+impl Neighbours for Vec3<i64> {
+    fn neighbours(&self) -> Vec<Self> {
+        NEIGHBOUR_OFFSETS_3.iter().map(|x| *self + *x).collect()
+    }
+}
+
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Add, Mul)]
 pub struct Vec4<T> {
     pub x: T,
@@ -51,6 +107,18 @@ pub struct Vec4<T> {
 impl<T> Vec4<T> {
     pub fn new(x: T, y: T, z: T, w: T) -> Self {
         Vec4 { x, y, z, w }
+    }
+}
+
+impl FromPair for Vec4<i64> {
+    fn from_pair(x: i64, y: i64) -> Self {
+        Vec4::new(x, y, 0, 0)
+    }
+}
+
+impl Neighbours for Vec4<i64> {
+    fn neighbours(&self) -> Vec<Self> {
+        NEIGHBOUR_OFFSETS_4.iter().map(|x| *self + *x).collect()
     }
 }
 
