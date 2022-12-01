2e5f077f4332993b0da1fb18ef0d1a5c7c5956cd
Adam Kelly <adam@cthulahoops.org>
2022-12-01 08:34:33 +0000

    2022 day1

---
+++ 2022/day1.scm
@@ -0,0 +1,29 @@
+(use-modules (ice-9 textual-ports))
+(use-modules (ice-9 rdelim))
+(use-modules (ice-9 regex))
+(use-modules (srfi srfi-1))
+
+(define (read-blocks) (gather-list read-block null?))
+(define (read-block) (map string->number (gather-list read-line block-end?)))
+(define (block-end? line) (or (eof-object? line) (string-null? line)))
+
+(define (gather-list get-next end?)
+  (let loop ((item (get-next)) (items (list)))
+    (if (end? item)
+        (reverse items)
+        (loop (get-next) (cons item items))
+        )))
+
+(define (sum items) (fold + 0 items))
+(define (maximum items) (fold max 0 items))
+
+(define blocks (with-input-from-file "input/1" read-blocks))
+
+(define sorted-totals (sort (map sum blocks) >))
+
+(display "Part 1: ")
+(display (car sorted-totals))
+(newline)
+(display "Part 2: ")
+(display (sum (take sorted-totals 3)))
+(newline)
