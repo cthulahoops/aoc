1b6b5ceed7036582ca2ebc1228dfcda1556dbb9d
Adam Kelly <adam@cthulahoops.org>
2022-12-09 10:29:13 +0000

    with-input

--- 2022/run.scm
+++ 2022/run.scm
@@ -6,11 +6,14 @@
 (define (script day) (string-append "day" day ".scm"))
 (define (input day) (string-append "input/" day))
 
+(define (with-input day f)
+  (with-input-from-file (input day) f))
+
 (define (main args)
   (let* (
       (day (cadr args))
       (script (script day))
       (input (input day)))
     (load script)
-    (format #t "Part 1: ~s\n" (with-input-from-file input part1))
-    (format #t "Part 2: ~s\n" (with-input-from-file input part2))))
+    (format #t "Part 1: ~s\n" (with-input day part1))
+    (format #t "Part 2: ~s\n" (with-input day part2))))
