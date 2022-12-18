2c8cc307d03ecddc80ccdc3cc3a6bd606742a0ca
Adam Kelly <adam@cthulahoops.org>
2022-12-18 07:59:35 +0000

    Day 18.

--- 2022/day18.scm
+++ 2022/day18.scm
@@ -1,6 +1,7 @@
 (add-to-load-path ".")
 (use-modules (srfi srfi-1))
 (use-modules (srfi srfi-9 gnu))
+(use-modules (ice-9 q))
 (use-modules (aoc))
 
 (define-immutable-record-type <point3>
@@ -29,11 +30,42 @@
 (define (list->set items) (let ((set (make-hash-table))) (for-each (lambda (item) (hash-set! set item #t)) items) set))
 
 (define (uncovered scan point)
-  (- 6 (count-where (partial hash-ref scan) (neighbours point))))
+  (count-where (lambda (p) (not (hash-ref scan p))) (neighbours point)))
+(define (covered scan point)
+  (count-where (lambda (p) (hash-ref scan p)) (neighbours point)))
+
+(define (fillable? scan p)
+  (not (or (< (point-x p) -1)
+           (> (point-x p) 25)
+           (< (point-y p) -1)
+           (> (point-y p) 25)
+           (< (point-z p) -1)
+           (> (point-z p) 21)
+           (hash-ref scan p))))
+
+(define (flood-fill scan start)
+  (let ((water (make-hash-table))
+        (q (make-q)))
+    (enq! q start)
+    (let loop ()
+      (if (q-empty? q)
+        water
+        (let ((current (deq! q)))
+          (if (hash-ref water current)
+                (loop)
+              (begin
+                (hash-set! water current #t)
+                (for-each (partial enq! q) (filter (partial fillable? scan) (neighbours current)))
+                (loop))
+              ))))))
 
 (define (part1)
   (let* ((input (read-input))
          (scan (list->set input)))
   (sum (map (partial uncovered scan) input))))
 
-(define (part2) 0)
+(define (part2)
+  (let* ((input (read-input))
+         (scan (list->set input))
+         (water (flood-fill scan (make-point3 -1 -1 -1))))
+    (sum (map (partial covered water) input))))
