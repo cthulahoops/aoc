e85ce20ff95f547780ce7e8a430a2c3bdbe30853
Adam Kelly <adam@cthulahoops.org>
2022-12-20 22:46:25 +0000

    Day 20 - massively simplify.

--- 2022/day20.scm
+++ 2022/day20.scm
@@ -1,50 +1,16 @@
 (add-to-load-path ".")
 (use-modules (aoc))
-(use-modules (srfi srfi-9 gnu))
 (use-modules (srfi srfi-1))
 
-(define-immutable-record-type <circular>
-  (make-circular offset head tail)
-  circular?
-  (offset circular-offset)
-  (head circular-head)
-  (tail circular-tail))
+(define (mix value items)
+  (let ((source-position (list-index (lambda (x) (equal? x value)) items)))
+    (list-insert (list-delete-ref items source-position) (modulo (+ source-position (cdr value)) 4999) value)))
 
-(define (circular lst) (make-circular 0 lst '()))
-(define (circular-car circular) (car (circular-head circular)))
-(define (circular-cdr circular)
-  (if (null? (cdr (circular-head circular)))
-        (make-circular 0
-                       (reverse (cons (car (circular-head circular)) (circular-tail circular)))
-                       (list))
-        (make-circular (1+ (circular-offset circular))
-                       (cdr (circular-head circular))
-                       (cons (car (circular-head circular)) (circular-tail circular)))))
+(define (list-delete-ref items index)
+  (append (take items index) (drop items (+ index 1))))
 
-(define (circular-pop-cdr circular)
-  (if (null? (cdr (circular-head circular)))
-        (make-circular 0 (reverse (circular-tail circular)) (list))
-        (make-circular (circular-offset circular) (cdr (circular-head circular)) (circular-tail circular))))
-
-(define (circular-scan circular value)
-  (if (equal? (circular-car circular) value)
-      circular
-      (circular-scan (circular-cdr circular) value)))
-
-(define (circular-scan-to-offset circular offset)
-  (if (= offset (circular-offset circular))
-      circular
-      (circular-scan-to-offset (circular-cdr circular) offset)))
-
-(define (circular-insert circular value)
-  (make-circular (circular-offset circular) (cons value (circular-head circular)) (circular-tail circular)))
-
-(define (mix value circular)
-  (let ((source-position (circular-scan circular value)))
-    (circular-insert (circular-scan-to-offset (circular-pop-cdr source-position) (modulo (+ (circular-offset source-position) (cdr value)) 4999)) value)))
-
-
-(define (circular->list circular) (append (circular-head circular) (reverse (circular-tail circular))))
+(define (list-insert items index value)
+  (append (take items index) (cons value (drop items index))))
 
 (define (grove-coordinates output)
   (let ((output-size (length output))
@@ -56,15 +22,14 @@
 
 (define (part1)
   (let* ((input (enumerate (map string->number (read-lines))))
-         (mixed (fold mix (circular input) input))
-         (output (map cdr (circular->list mixed)))
+         (mixed (fold mix input input))
+         (output (map cdr mixed))
         )
     (grove-coordinates output)))
 
 (define (part2)
   (let* ((input (enumerate (map (lambda (x) (* 811589153 x)) (map string->number (read-lines)))))
-         (circular (circular input))
-         (mixed (apply-n-times 10 (lambda (x) (fold mix x input)) circular))
-         (output (map cdr (circular->list mixed)))
+         (mixed (apply-n-times 10 (lambda (x) (fold mix x input)) input))
+         (output (map cdr mixed))
         )
     (grove-coordinates output)))
