54593001b95ba391a9c1ac794f9ed9d3792e0370
Adam Kelly <adam@cthulahoops.org>
2022-12-22 23:20:11 +0000

    Day 22 - part 2.

--- 2022/day22.scm
+++ 2022/day22.scm
@@ -78,7 +78,7 @@
 
 (define (ahead-of bounds position)
   (match (hash-ref bounds position) (#f (make-position (point+ (position-location position) (step-direction (position-facing position))) (position-facing position)))
-                                    ((? position? wrap-position) wrap-position)))
+                                    ((? position? wrap-position) (display (list 'wrapped wrap-position)) (newline) wrap-position)))
 
 (define step-direction
   (match-lambda (0 (make-point 1 0))
@@ -93,6 +93,8 @@
                 (($ <point> 0 -1) 3)))
 
 (define (step grid bounds position)
+; (display (list 'step position (ahead-of bounds position)))
+ ; (newline)
   (let ((next (ahead-of bounds position)))
     (match (hash-ref grid (position-location next)) (#\. next)
                                                     (#\# position))))
@@ -117,5 +119,99 @@
         )
     (password final)
     ))
+(define (part1) 0)
 
-(define (part2) 0)
+; (define (corner-vec grid corner)
+;   (find (lambda (v) (not (hash-ref grid (point+ corner v)))) (list (make-point 1 1) (make-point 1 -1) (make-point -1 -1) (make-point -1 1))))
+
+; (define corners
+;   (list (make-point 9 5) (make-point 9 8) (make-point 12 9)))
+
+(define (iterate-n f init count)
+  (if (= 0 count)
+      '()
+      (cons init (iterate-n f (f init) (1- count)))))
+
+(define (turn-left vec) (step-direction (modulo (- (vec-to-facing vec) 1) 4)))
+(define (turn-right vec) (step-direction (modulo (+ (vec-to-facing vec) 1) 4)))
+
+(define (choose-next grid point vec)
+  (let* (
+        (left (turn-left vec))
+        (right (turn-right vec))
+        (ahead (point+ point vec))
+        (inside-left (point+ ahead left))
+        (outside-right (point+ point right)))
+    (cond
+      ((hash-ref grid inside-left) (cons inside-left left))
+      ((hash-ref grid ahead) (cons ahead vec))
+      ((hash-ref grid outside-right) (cons point right)))))
+
+; (define assembley-plan (list 5 4 1 2 2 3 3 1 4 5 6 6 7 7))
+; (define cube-size 50)
+; EXAMPLE
+; (define assembley-plan (list 1 2 3 3 2 4 5 6 6 5 4 1 7 7))
+; (define cube-size 4)
+; REAL
+(define assembley-plan (list 1 2 3 4 4 3 5 5 2 1 6 7 7 6))
+(define cube-size 50)
+
+(define (follow-boundary grid start vec n)
+  (if (= n 0)
+    '()
+    (let* ((first-edge (iterate-n (lambda (p) (point+ p vec)) start cube-size))
+           (next (choose-next grid (last first-edge) vec))
+           (next-start (car next))
+           (next-vec (cdr next)))
+      (cons (map (partial list (turn-right vec)) first-edge) (follow-boundary grid next-start next-vec (- n 1))))))
+    
+; (define (generate-boundary-mapping grid corner)
+;   (let* ((vec (corner-vec grid corner))
+;          (v1 (make-point 0 (point-y vec)))
+;          (v2 (make-point (point-x vec) 0)))
+;     (map-in-order cons (follow-boundary grid corner v1) (follow-boundary grid corner v2))))
+
+
+
+(define (assemble boundary)
+  (pipe>
+    (sort (map cons assembley-plan boundary) (lambda (x y) (< (car x) (car y))))
+    (map cdr)
+    (chunk 2)
+    (append-map (lambda (parts) (map list (first parts) (reverse (second parts)))))
+    (display-lines)
+    )
+  )
+
+(define (point-back point) (make-point (- (point-x point)) (- (point-y point))))
+
+(define (create-boundary boundary)
+  (display-lines boundary)
+  (newline)
+  (let* ((bounds (make-hash-table))
+         (add-bound! (lambda (p1 v1 p2 v2) (hash-set! bounds (make-position p1 (vec-to-facing v1)) (make-position p2 (vec-to-facing v2))))))
+    (map (match-lambda (((v1 p1) (v2 p2)) (add-bound! p1 (point-back v1) p2 v2)
+                                          (add-bound! p2 (point-back v2) p1 v1))) boundary)
+    (display-lines (hash-keys bounds))
+    (newline)
+    bounds))
+
+(define (part2)
+  (let* (
+        (grid (parse-board (read-block)))
+        (instructions (parse-instructions (car (read-block))))
+        (start-position (make-position (make-point (min-x grid 1) 1) 0))
+        (boundary (assemble (follow-boundary grid (position-location start-position) (make-point 1 0) 14)))
+        (bounds (create-boundary boundary))
+        (final (fold (lambda (instruction position) (display (list position instruction)) (newline) (apply-instruction grid bounds position instruction)) start-position instructions))
+        )
+    (display final)
+    (newline)
+    (display start-position)
+    (password final)
+    ; (pipe> 
+    ;   bond
+    ;   )
+    ; (newline)
+    ;0 
+    ))
