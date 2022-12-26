cefcdf5e4fd1852a4c7fae736b652d6cab8081a9
Adam Kelly <adam@cthulahoops.org>
2022-12-26 14:29:01 +0000

    Day 1 - clean-up and simplify.

--- 2022/day1.scm
+++ 2022/day1.scm
@@ -1,13 +1,8 @@
+(add-to-load-path ".")
 (use-modules (srfi srfi-1))
 (use-modules (aoc))
 
-(define (keep-biggest new items) (cdr (sort (cons new items) <)))
-
-(define (blocks)
-  (pipe>
-    (read-blocks)
-    (map (lambda (line) (map string->number line)))))
-
+(define (blocks) (map (lambda (line) (map string->number line)) (read-blocks)))
 (define (sorted-totals) (sort (map sum (blocks)) >))
 
 (define (part1) (car (sorted-totals)))
