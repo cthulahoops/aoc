0afd9ec0c1afdfd327f6391a62fdff1adc5d6c35
Adam Kelly <adam@cthulahoops.org>
2025-12-03 13:45:11 +0000

    Configure Vite to build all HTML pages

--- 2025/vite.config.js
+++ 2025/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        day1: resolve(__dirname, "day1.html"),
+        day2: resolve(__dirname, "day2.html"),
+        day3: resolve(__dirname, "day3.html"),
+      },
+    },
+  },
 });
