1eaf65fb193264d08bd70f9c70c9366bdfc97e13
Adam Kelly <adam@cthulahoops.org>
2025-12-06 08:25:11 +0000

    2025 Day 6 - Part 2.

--- 2025/src/day6.tsx
+++ 2025/src/day6.tsx
@@ -18,10 +18,23 @@ const OPERATORS = {
   },
 };
 
+type Operator = keyof typeof OPERATORS;
+
 async function solve(input: string) {
   console.log("Solving problem");
-  const homework = parseInput(input);
+  const lines = parseLines(input);
+  const totals1 = part1(lines);
+  const totals2 = part2(lines);
 
+  return {
+    part1: sum(totals1),
+    part2: sum(totals2),
+    output: { totals1, totals2 },
+  };
+}
+
+function part1(lines: string[]) {
+  const homework = lines.map((x) => x.trim().split(/ +/));
   const totals = [];
 
   for (let i = 0; i < homework[0].length; i++) {
@@ -35,17 +48,50 @@ async function solve(input: string) {
 
     totals.push(result);
   }
-
-  return {
-    part1: sum(totals),
-    part2: 0,
-    output: totals,
-  };
+  return totals;
 }
 
-function parseInput(input: string): string[][] {
-  const lines = parseLines(input);
-  return lines.map((x) => x.trim().split(/ +/));
+function part2(lines: string[]) {
+  const sheet = [];
+  for (let i = 0; i < lines[0].length; i++) {
+    const column = [];
+    for (let j = 0; j < lines.length; j++) {
+      column.push(lines[j][i]);
+    }
+    sheet.push(column.join("").trim());
+  }
+  const sums = splitOn(sheet, "");
+  sums.forEach((x) => x.reverse());
+
+  const totals = [];
+  for (const sum of sums) {
+    const operator = OPERATORS[lastChar(last(sum)) as Operator];
+    const terms = sum.map((x) => Number(x.replace(/[ +*]/g, "")));
+    const total = terms.reduce(operator.operation, operator.identity);
+    totals.push(total);
+  }
+
+  return totals;
+}
+
+function lastChar(items: string): string {
+  return items[items.length - 1];
+}
+
+function last<T>(items: T[]): T {
+  return items[items.length - 1];
+}
+
+function splitOn<T>(items: T[], splitter: T) {
+  const result: T[][] = [[]];
+  for (const item of items) {
+    if (item === splitter) {
+      result.push([]);
+    } else {
+      result[result.length - 1].push(item);
+    }
+  }
+  return result;
 }
 
 function Solution() {
